385ecd17a482bd29a276ee602f82e25fd691a3eb
Daniel Szabo <szdani91@gmail.com>
2015-02-25 17:55:57 +0100

    negyedik teszt kesz

--- src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
+++ src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
@@ -3,10 +3,12 @@ package com.wcs.fizzbuzz;
 public class FizzBuzzer {
 
     public String execute(int number) {
-        if (number % 3 == 0) {
-            return "fizz";
+        if((number % 3 == 0) && (number % 5 == 0)) {
+            return "fizzbuzz";
         } else if(number % 5 == 0) {
             return "buzz";
+        } else if(number % 3 == 0){
+            return "fizz";
         } else {
             return String.valueOf(number);
         }
