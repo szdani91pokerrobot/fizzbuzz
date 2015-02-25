3e7675c58b11252228052b9ebf7372e7ccb20838
Daniel Szabo <szdani91@gmail.com>
2015-02-25 17:51:47 +0100

    masodik teszt kesz

--- src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
+++ src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
@@ -5,6 +5,8 @@ public class FizzBuzzer {
     public String execute(int number) {
         if (number % 3 == 0) {
             return "fizz";
+        } else if(number % 5 == 0) {
+            return "buzz";
         } else {
             return "";
         }
